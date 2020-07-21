0fbd7a5f96a05f689bdc167602d4207e4afd851a
AlexeyMonarh <monarhweb@gmail.com>
2020-07-21 18:34:05 +0300

    js_final

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Form(close/open)
 let openForm = document.querySelector('.profile__info-edit-button');
 let closeForm = document.querySelector('.form__close-icon');
 let form = document.querySelector('.form');
@@ -6,17 +7,25 @@ function toggleForm() {
   form.classList.toggle('form_open');
 }
 
-openForm.addEventListener('click', toggleForm)
+openForm.addEventListener('click', toggleForm);
 
-closeForm.addEventListener('click', toggleForm)
+closeForm.addEventListener('click', toggleForm);
 
+// Form(edit)
 let formElement = document.querySelector('.form');
+let profileName = document.querySelector('.profile__info-name');
+let profileStatus = document.querySelector('.profile__info-status');
 
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
+function formSubmitHandler(evt) {
+  evt.preventDefault();
 
   let nameInput = document.querySelector('.form__input-name');
-  let jobInput = document.querySelector('');
+  let jobInput = document.querySelector('.form__input-job');
 
-  nameInput.hasAttribute('value');
+  profileName.textContent = nameInput.value;
+  profileStatus.textContent = jobInput.value;
+
+  toggleForm();
 }
+
+formElement.addEventListener('submit', formSubmitHandler);
