c4cbe36cbd206fc18918f708b2b03a8c99305b82
AlexeyMonarh <monarhweb@gmail.com>
2020-07-21 23:07:27 +0300

    js_reveu

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Form(close/open)
 let openForm = document.querySelector('.profile__info-edit-button');
-let closeForm = document.querySelector('.form__close-icon');
-let form = document.querySelector('.form');
+let closeForm = document.querySelector('.popup__close-icon');
+let form = document.querySelector('.popup');
 
 function toggleForm() {
-  form.classList.toggle('form_open');
+  form.classList.toggle('popup_open');
 }
 
 openForm.addEventListener('click', toggleForm);
@@ -12,15 +12,15 @@ openForm.addEventListener('click', toggleForm);
 closeForm.addEventListener('click', toggleForm);
 
 // Form(edit)
-let formElement = document.querySelector('.form');
+let formElement = document.querySelector('.popup');
 let profileName = document.querySelector('.profile__info-name');
 let profileStatus = document.querySelector('.profile__info-status');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameInput = document.querySelector('.form__input-name');
-  let jobInput = document.querySelector('.form__input-job');
+  let nameInput = document.querySelector('.popup__input-name');
+  let jobInput = document.querySelector('.popup__input-job');
 
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
