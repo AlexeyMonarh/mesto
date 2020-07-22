8d51956af0c9302895def899d027d1c38f3413ab
AlexeyMonarh <monarhweb@gmail.com>
2020-07-22 16:22:49 +0300

    Проектная_4_2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
-// Form(close/open)
+
 let openForm = document.querySelector('.profile__info-edit-button');
 let closeForm = document.querySelector('.popup__close-icon');
 let form = document.querySelector('.popup');
 
+let formElement = document.querySelector('.popup');
+let profileName = document.querySelector('.profile__info-name');
+let profileStatus = document.querySelector('.profile__info-status');
+let nameInput = document.querySelector('.popup__input-name');
+let jobInput = document.querySelector('.popup__input-job');
+
+nameInput.value = profileName.textContent;
+jobInput.value = profileStatus.textContent;
+
 function toggleForm() {
   form.classList.toggle('popup_open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileStatus.textContent;
 }
 
 openForm.addEventListener('click', toggleForm);
 
 closeForm.addEventListener('click', toggleForm);
 
-// Form(edit)
-let formElement = document.querySelector('.popup');
-let profileName = document.querySelector('.profile__info-name');
-let profileStatus = document.querySelector('.profile__info-status');
+
+
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  let nameInput = document.querySelector('.popup__input-name');
-  let jobInput = document.querySelector('.popup__input-job');
 
   profileName.textContent = nameInput.value;
   profileStatus.textContent = jobInput.value;
