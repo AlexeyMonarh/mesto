cf3160539d03b723357774a63339847f6f77f9f7
AlexeyMonarh <monarhweb@gmail.com>
2020-07-24 15:29:34 +0300

    trash_elements

--- js/script.js
+++ js/script.js
@@ -9,6 +9,50 @@ let profileStatus = document.querySelector('.profile__info-status');
 let nameInput = document.querySelector('.popup__input-name');
 let jobInput = document.querySelector('.popup__input-job');
 
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach((data) => {
+  const elementsTemplate = document.querySelector('.elements__template').content.querySelector('.elements__element');
+  const elementsElement = elementsTemplate.cloneNode(true);
+
+  const elementsImage = elementsElement.querySelector('.elements__element-img');
+  const elementsTitle = elementsElement.querySelector('.elements__element-title');
+  const elementsLikeButton = elementsElement.querySelector('.elements__element-like');
+  const elementsDeleteButton = elementsElement.querySelector('.elements__element-delete-button');
+
+  elementsTitle.textContent = data.name;
+  elementsImage.src = data.link;
+
+  const elements = document.querySelector('.elements');
+  elements.prepend(elementsElement);
+
+})
+
 nameInput.value = profileName.textContent;
 jobInput.value = profileStatus.textContent;
 
@@ -21,9 +65,6 @@ function toggleForm() {
 openForm.addEventListener('click', toggleForm);
 
 closeForm.addEventListener('click', toggleForm);
-
-
-
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
