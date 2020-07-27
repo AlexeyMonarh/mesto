6d5e0db71bce01d41eecb638118267978546e742
AlexeyMonarh <monarhweb@gmail.com>
2020-07-27 13:14:52 +0300

    Проектная_работа_5_2

--- js/script.js
+++ js/script.js
@@ -29,32 +29,80 @@ const openAddButton = document.querySelector('.profile__add-button');
 const elementsTemplate = document.querySelector('.elements__template').content.querySelector('.elements__element');
 const elements = document.querySelector('.elements');
 
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+
+function toggleForm(form) {
+  form.classList.toggle('popup_open');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileStatus.textContent;
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = nameInput.value;
+  profileStatus.textContent = jobInput.value;
+
+  toggleForm(editProfileModal);
+}
+
+// function addElementQueue(data) {
+//   elements.append(createElement(data));
+// }
+
+function addElementSubmitHandler(evt) {
+  evt.preventDefault();  
+  renderElement({name: placeInput.value, link: linkInput.value});
+  
+  toggleForm(addElementModal);
+  placeInput.value = '';
+  linkInput.value = '';
+}
+
+function handleLikeClick(elementsLikeButton) {
+  elementsLikeButton.classList.toggle('elements__element-like_active');
+}
+
+function handleDeleteClick(elementsDeleteButton) {
+  const listElement = elementsDeleteButton.closest('.elements__element');
+  listElement.remove();
+}
+
+function renderElement(data) {
+  elements.prepend(createElement(data));
+}
+
+function createElement(data) {
+  const card = elementsTemplate.cloneNode(true);
+  const elementsImage = card.querySelector('.elements__element-img');
+  const elementsTitle = card.querySelector('.elements__element-title');
+  const elementsLikeButton = card.querySelector('.elements__element-like');
+  const elementsDeleteButton = card.querySelector('.elements__element-delete-button');
+
+  elementsLikeButton.addEventListener('click', () => {
+    handleLikeClick(elementsLikeButton);
+  });
+
+  elementsDeleteButton.addEventListener('click', () => {
+    handleDeleteClick(elementsDeleteButton);
+  });
+
+  elementsImage.addEventListener('click', () => {
+    handleImageClick();
+  });
+  
+  function handleImageClick() {
+    
+    popupOpenTitle.textContent = data.name;
+    popupOpenImage.src = data.link;
+  
+    toggleForm(imageModal);
   }
-];
+
+  elementsTitle.textContent = data.name;
+  elementsImage.src = data.link;
+
+  return card;
+};
 
 popupInputs.addEventListener('submit', formSubmitHandler);
 addElement.addEventListener('submit', addElementSubmitHandler);
@@ -84,74 +132,4 @@ jobInput.value = profileStatus.textContent;
 
 initialCards.forEach((data) => {
   renderElement(data);
-})
-
-function toggleForm(form) {
-  form.classList.toggle('popup_open');
-  nameInput.value = profileName.textContent;
-  jobInput.value = profileStatus.textContent;
-}
-
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-
-  profileName.textContent = nameInput.value;
-  profileStatus.textContent = jobInput.value;
-
-  toggleForm(editProfileModal);
-}
-
-function addElementSubmitHandler(evt) {
-  evt.preventDefault();  
-  renderElement({name: placeInput.value, link: linkInput.value});
-  
-  toggleForm(addElementModal);
-  placeInput.value = '';
-  linkInput.value = '';
-}
-
-function handleLikeClick(elementsLikeButton) {
-  elementsLikeButton.classList.toggle('elements__element-like_active');
-}
-
-function handleDeleteClick(elementsDeleteButton) {
-  const listElement = elementsDeleteButton.closest('.elements__element');
-  listElement.remove();
-}
-
-function renderElement(data) {
-  elements.append(createElement(data));
-}
-
-function createElement(data) {
-  const elementsElement = elementsTemplate.cloneNode(true);
-  const elementsImage = elementsElement.querySelector('.elements__element-img');
-  const elementsTitle = elementsElement.querySelector('.elements__element-title');
-  const elementsLikeButton = elementsElement.querySelector('.elements__element-like');
-  const elementsDeleteButton = elementsElement.querySelector('.elements__element-delete-button');
-
-  elementsLikeButton.addEventListener('click', () => {
-    handleLikeClick(elementsLikeButton);
-  });
-
-  elementsDeleteButton.addEventListener('click', () => {
-    handleDeleteClick(elementsDeleteButton);
-  });
-
-  elementsImage.addEventListener('click', () => {
-    handleImageClick();
-  });
-  
-  function handleImageClick() {
-    
-    popupOpenTitle.textContent = data.name;
-    popupOpenImage.src = data.link;
-  
-    toggleForm(imageModal);
-  }
-
-  elementsTitle.textContent = data.name;
-  elementsImage.src = data.link;
-
-  return elementsElement;
-}
+});
